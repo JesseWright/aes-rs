a8e288818e95deacd5a2eb959b47dff00dacda3b
Jinwook Kim <jininja7@gmail.com>
2017-10-07 00:29:05 -0500

    translated ShiftBytes, InverseMixColumns, MixColumn, AddRoundKey, and working on DecryptCBC now.

--- src/crypto/aes.rs
+++ src/crypto/aes.rs
@@ -1,3 +1,89 @@
+fn DecryptCBC(cipherText: [char], key: [char], keySize: KeySize)
+{
+//    let a:[char; 1] = [];
+    if cipherText == null
+        {
+            return null;
+        }
+
+    if cipherText.len() == 0
+        {
+            return char[0];
+        }
+
+    let blockCount:i32 =
+}
+
+fn AddRoundKey(block: [char], expandedKey:[char], round:i32)
+{
+    let j:i32 = round * BLOCK_SIZE;
+    for i in 0..BLOCK_SIZE
+        {
+            block[i] ^= expandedKey[j + i];
+            block[i] &= 0xFF;
+        }
+}
+
+/* Courtesy http://www.samiam.org/mix-column.html */
+fn MixColumns(r: [char])
+{
+    let a:[char; 4] = [];
+    let b:[char; 4] = [];
+    let c:char;
+    let h:char;
+
+    for c in 0..4
+        {
+            a[c] = r[c];
+            h = (r[c] & 0x80) as char;
+            b[c] = ((r[c] << 1) & 0xFF) as char;
+
+            if h == 0x80
+                {
+                    /* Rijndael's Galois field */
+                    b[c] ^= 0x1b;
+                    b[c] &= 0xFF;
+                }
+        }
+    r[0] = ((b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]) & 0xFF) as char;
+    r[1] = ((b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]) & 0xFF) as char;
+    r[2] = ((b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]) & 0xFF) as char;
+    r[3] = ((b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]) & 0xFF) as char;
+}
+
+/* Courtesy http://www.samiam.org/mix-column.html */
+fn InverseMixColumns(r: [char])
+{
+    let a: [char; 4] = [];
+
+    let c: char;
+
+    for c in 0..4
+        {
+            a[c] = r[c];
+        }
+
+    r[0] = (char)((GaloisMultiply(a[0], 14 as char)
+        ^ GaloisMultiply(a[3], 9 as char)
+        ^ GaloisMultiply(a[2], 13 as char)
+        ^ GaloisMultiply(a[1], 11 as char)) & 0xFF);
+
+    r[1] = (char)((GaloisMultiply(a[1], 14 as char)
+        ^ GaloisMultiply(a[0], 9 as char)
+        ^ GaloisMultiply(a[3], 13 as char)
+        ^ GaloisMultiply(a[2], 11 as char)) & 0xFF);
+
+    r[2] = (char)((GaloisMultiply(a[2], 14 as char)
+        ^ GaloisMultiply(a[1], 9 as char)
+        ^ GaloisMultiply(a[0], 13 as char)
+        ^ GaloisMultiply(a[3], 11 as char)) & 0xFF);
+
+    r[3] = (char)((GaloisMultiply(a[3], 14 as char)
+        ^ GaloisMultiply(a[2], 9 as char)
+        ^ GaloisMultiply(a[1], 13 as char)
+        ^ GaloisMultiply(a[0], 11 as char)) & 0xFF);
+}
+
 fn ShiftBytes(array: [char], shiftAmount: i32)
 {
     let reducedShiftAmount = shiftAmount.abs() % array.len();
@@ -7,35 +93,32 @@ fn ShiftBytes(array: [char], shiftAmount: i32)
             return;
         }
 
-    let mut i:i32;
-    let mut b:char;
-    let mut e:char;
+    let mut i: i32;
+    let mut b: char;
+    let mut e: char;
 
-    for count in reducedShiftAmount
+    for count in 0..reducedShiftAmount
         {
             i = 0;
             if shiftAmount > 0
                 {
                     b = array[i];
                     i = 1;
-                    for (index, value) in array.len().enumerate()
+                    for index in i..array.len()
                         {
-                            array[i - 1] = array[i];
+                            array[index - 1] = array[index];
                         }
 
-                    array[i - 1] = b;
-                }
-            else 
-            { 
+                    array[index - 1] = b;
+                } else {
                 e = array[array.len() - 1];
                 i = 1;
-                for i in array.len() - 1 {
-                    array[i] = array[i ]
+                for index in i..array.len() - 1 {
+                    array[index] = array[index - 1]
                 }
             }
         }
 }
-
 
 fn ShiftRows(cipherBlock: [char])
 {
@@ -44,26 +127,26 @@ fn ShiftRows(cipherBlock: [char])
             return;
         }
 
-    let rowCount:i32 = (i32) (cipherBlock.len() / (f32) BLOCK_WIDTH).ceil();
+    let rowCount: i32 = (i32)(cipherBlock.len() / (f32)
+    BLOCK_WIDTH).ceil();
 
-    let mut row:[char];
+    let mut row: [char];
 
-    for i in rowCount
-    {
-        row = [BLOCK_WIDTH:char];
-        for j in row.len()
+    for i in 0..rowCount
         {
-            row[j] = cipherBlock[i + j * row.len()];
+            row = [BLOCK_WIDTH: char];
+            for j in row.len()
+                {
+                    row[j] = cipherBlock[i + j * row.len()];
+                }
+
+            ShiftByte(row, i);
+
+            for j in 0..row.len()
+                {
+                    cipherBlock[i + j * row.len()] = row[j];
+                }
         }
-
-        ShiftByte(row, i);
-
-        for j in row.len()
-            {
-                cipherBlock[i + j * row.len()] = row[j];
-            }
-    }
-
 }
 
 fn InverseShiftRows(cipherBlock: [char])
@@ -73,32 +156,32 @@ fn InverseShiftRows(cipherBlock: [char])
             return;
         }
 
-    let rowCount:i32 = (i32) (cipherBlock.len() / (f32)BLOCK_WIDTH).ceil(); // cast BLOCK_WIDTH into a double
+    let rowCount: i32 = (i32)(cipherBlock.len() / (f32)
+    BLOCK_WIDTH).ceil(); // cast BLOCK_WIDTH into a double
 
-    let mut row:[char];
+    let mut row: [char];
 
-    for i in rowCount
+    for i in 0..rowCount
         {
-            row = [BLOCK_WIDTH:char];
-            for j in row.len()
+            row = [BLOCK_WIDTH: char];
+            for j in 0..row.len()
                 {
                     row[j] = cipherBlock[i + j * row.len()];
                 }
             ShiftBytes(row, -i);
-            for j in row.len()
+            for j in 0..row.len()
                 {
                     cipherBlock[i + j * row.len()] = row[j];
                 }
         }
 }
 
-
 fn SubBytes(cipherBlock: [char])
 {
-    for byteIndex in 0...cipherBlock.len()
+    for byteIndex in 0..cipherBlock.len()
         {
-            let leftNibble:i32 = (cipherBlock[byteIndex] & 0xFF) >> 4;
-            let rightNibble:i32 = cipherBlock[byteIndex] & 0x0f;
+            let leftNibble: i32 = (cipherBlock[byteIndex] & 0xFF) >> 4;
+            let rightNibble: i32 = cipherBlock[byteIndex] & 0x0f;
 
             cipherBlock[byteIndex] = AES.SBOX[leftNibble][rightNibble];
         }
@@ -108,8 +191,8 @@ fn InverseSubBytes(cipherBlock: [char])
 {
     for byteIndex in 0..cipherBlock.len()
         {
-            let leftNibble:i32 = (cipherBlock[byteIndex] & 0xFF) >> 4;
-            let rightNibble:i32 = cipherBlock[byteIndex] & 0x0f;
+            let leftNibble: i32 = (cipherBlock[byteIndex] & 0xFF) >> 4;
+            let rightNibble: i32 = cipherBlock[byteIndex] & 0x0f;
 
             cipherBlock[byteIndex] = AES.INVERSE_SBOX[leftNibble][rightNibble];
         }
